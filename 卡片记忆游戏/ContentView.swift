10c1e149e4993c8132950a49e4bbddf191832bd2
杨海 <yh13169228317@icloud.com>
2022-02-25 20:45:56 +0800

    Initial Commit

---
+++ 卡片记忆游戏/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  卡片记忆游戏
+//
+//  Created by 杨海 on 2022/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 卡片记忆游戏/______App.swift
@@ -0,0 +1,17 @@
+//
+//  ______App.swift
+//  卡片记忆游戏
+//
+//  Created by 杨海 on 2022/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ______App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
